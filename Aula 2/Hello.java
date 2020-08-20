fc1e2f1080f8310cf14c6499ddaf46566ec116fb
Paloma Kimberly <57427551+Novetres@users.noreply.github.com>
2020-08-20 20:37:30 -0300

    Adicionei arquivos da aula 2

---
+++ Aula 2/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author palom
+ * Hello Wordl
+  */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
