8a7373df27aed367ac4f0ba96014b581b6f33a8e
Paloma Kimberly <57427551+Novetres@users.noreply.github.com>
2020-08-27 19:28:08 -0300

    Adicionei o projeto livre

---
+++ Projeto livre/Adedonha.java
@@ -0,0 +1,80 @@
+package exercicios;
+import java.util.Random;
+
+
+
+public class Adedonha {
+
+  public static void main(String[] args){
+	
+	   
+	    System.out.println(" == OOOoohh STOP ==");
+	    System.out.println("");
+	 
+		Random aleatorio = new Random (); 
+		int palavra = aleatorio.nextInt(12) + 1;  			  
+		if (palavra == 1)
+		System.out.println("   Desafio: " + "NOME");
+		else if (palavra  == 1) {
+		System.out.println("   Desafio: " + "COR");	
+		} else if (palavra == 2) {
+		System.out.println("   Desafio: " + "OBJETO");}
+		else if (palavra == 3) {	  				        
+		System.out.println ("   Desafio: " + "FRUTA");}
+		else if (palavra == 4) {	  				        
+		System.out.println ("   Desafio: " + "ANIMAL");}
+		else if (palavra == 5) {	  				        
+		System.out.println ("   Desafio: " + "CARRO");}
+		else if (palavra == 6) {	  				        
+		System.out.println ("   Desafio: " + "FILME");}
+		else if (palavra == 7) {	  				        
+		System.out.println ("   Desafio: " + "NOVELA");}
+		else if (palavra == 8) {	  				        
+		System.out.println ("   Desafio: " + "DESENHO");}
+		else if (palavra == 9) {	  				        
+		System.out.println ("   Desafio: " + "CIDADE");}
+		else if (palavra == 10) {	  				        
+		System.out.println ("   Desafio: " + "ESTADO");}
+		else if (palavra == 11) {	  				        
+		System.out.println ("   Desafio: " + "PAÍS");}
+		else if (palavra == 12) {	  				        
+		System.out.println ("   Desafio: " + "MINHA SOGRA É");}
+		System.out.println("    Com a ");
+		String letras = new String("ABCDEFGHIJKLMNOPQRSTUVXYZ");
+		Random numLetra = new Random();
+		byte posLetra = (byte)numLetra.nextInt(letras.length()-1);
+		System.out.println   ("   Letra: "+ letras.charAt(posLetra));
+        System.out.println   ("");  
+        
+        System.out.print(" == RESPOSTA ORAL ==");
+        }}
+
+         
+        	
+        	
+        
+        	
+        	
+
+        
+       
+     
+
+		   
+		   
+	   
+
+   
+
+  
+  
+
+
+  
+
+
+  
+
+
+   
+   
